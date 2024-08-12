8ca194f83b7f488aca1bc1abcfb8015ba2f4c5ae
Besmella Kazimy <59577434+BKazimy@users.noreply.github.com>
2024-08-12 17:22:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+var name = "Besmella";
+var year = new Date().getFullYear();
+
+var html = (
+  <div>
+    <p>Created by {name}</p>
+    <p>Copy right {year}</p>
+  </div>
+);
+
+ReactDOM.render(html, document.getElementById("root"));
